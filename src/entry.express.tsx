c424eb11f0e06bedffc0bd5fe0a490d4618540e8
aliyss <silyas.bieri@lagomio.com>
2023-09-04 19:49:19 +0200

    Added Headers.

--- src/entry.express.tsx
+++ src/entry.express.tsx
@@ -62,6 +62,17 @@ app.use(router);
 // Use Qwik City's 404 handler
 app.use(notFound);
 
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "OPTIONS,GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization",
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 // Start the express server
 app.listen(PORT, () => {
   /* eslint-disable */
