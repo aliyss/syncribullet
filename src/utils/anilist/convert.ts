9bda5dea5eca35b2d02d594aba077881b835ae68
aliyss <silyas.bieri@lagomio.com>
2024-06-15 18:48:41 +0200

    Fixed detailing for shows and movies

--- src/utils/anilist/convert.ts
+++ src/utils/anilist/convert.ts
@@ -1,4 +1,4 @@
-import { AnilistLibraryEntry } from "./types";
+import type { AnilistLibraryEntry } from "./types";
 
 export async function convertAnilistToCinemeta(
   type: "shows",
@@ -10,18 +10,56 @@ export async function convertAnilistToCinemeta(
 ) {
   try {
     if (type === "shows") {
-      return list.slice(options.skip, options.skip + 100).map((x) => {
-        return {
-          type: "series",
-          id: "anilist_" + x.media.id,
-          name: x.media.title.userPreferred,
-          poster: x.media.coverImage.large,
-          releaseInfo: (x.media.seasonYear || "").toString(),
-          genres: x.media.genres,
-          description: x.media.description,
-          background: x.media.bannerImage,
-        };
-      });
+      return list
+        .filter((x) => {
+          if (options.genre) {
+            if (x.media.format === "TV" && options.genre === "tv") {
+              return true;
+            } else if (x.media.format === "TV") {
+              return false;
+            }
+            if (x.media.format === "MOVIE" && options.genre === "movie") {
+              return true;
+            } else if (x.media.format === "MOVIE") {
+              return false;
+            }
+            if (x.media.format === "OVA" && options.genre === "ova") {
+              return true;
+            } else if (x.media.format === "OVA") {
+              return false;
+            }
+            if (x.media.format === "SPECIAL" && options.genre === "special") {
+              return true;
+            } else if (x.media.format === "SPECIAL") {
+              return false;
+            }
+            if (x.media.format === "MUSIC" && options.genre === "music video") {
+              return true;
+            } else if (x.media.format === "SPECIAL") {
+              return false;
+            }
+            if (x.media.format === "ONA" && options.genre === "ona") {
+              return true;
+            } else if (x.media.format === "ONA") {
+              return false;
+            }
+            return x.media.format === options.genre;
+          }
+          return true;
+        })
+        .slice(options.skip, options.skip + 100)
+        .map((x) => {
+          return {
+            type: "series",
+            id: "anilist_" + x.media.id,
+            name: x.media.title.userPreferred,
+            poster: x.media.coverImage.large,
+            releaseInfo: (x.media.seasonYear || "").toString(),
+            genres: x.media.genres,
+            description: x.media.description,
+            background: x.media.bannerImage,
+          };
+        });
     }
   } catch (e) {
     console.log(e);
--- src/utils/simkl/convert.ts
+++ src/utils/simkl/convert.ts
@@ -21,6 +21,7 @@ export async function convertSimklToCinemeta(
               }
               const data = (await getCinemetaMeta("movie", x.movie.ids.imdb))
                 ?.meta;
+
               return {
                 type: "movies",
                 id: x.movie.ids.imdb,
@@ -41,37 +42,76 @@ export async function convertSimklToCinemeta(
           }) as any[],
       );
     } else if (type === "shows") {
-      return list.shows
-        ?.slice(options.skip, options.skip ? options.skip + 100 : undefined)
-        .map((x) => {
-          return {
-            type: "series",
-            id: x.show.ids.imdb,
-            name: x.show.title,
-            releaseInfo: x.show.year,
-            poster: "https://simkl.in/posters/" + x.show.poster + "_0.jpg",
-            description: x.show.overview,
-          };
-        });
+      return await Promise.all(
+        list.shows
+          ?.slice(options.skip, options.skip ? options.skip + 50 : undefined)
+          .map(async (x) => {
+            try {
+              if (!x.show.ids.imdb) {
+                throw new Error("No imdb id");
+              }
+              const data = (await getCinemetaMeta("series", x.show.ids.imdb))
+                ?.meta;
+              return {
+                type: "series",
+                id: x.show.ids.imdb,
+                name: x.show.title,
+                releaseInfo: x.show.year,
+                poster: "https://simkl.in/posters/" + x.show.poster + "_0.jpg",
+                ...data,
+              };
+            } catch (e) {
+              return {
+                type: "series",
+                id: x.show.ids.imdb,
+                name: x.show.title,
+                releaseInfo: x.show.year,
+                poster: "https://simkl.in/posters/" + x.show.poster + "_0.jpg",
+              };
+            }
+          }) as any[],
+      );
     } else if (type === "anime") {
-      return list.anime
-        ?.filter((x) => {
-          if (options.genre) {
-            return x.anime_type === options.genre;
-          }
-          return true;
-        })
-        .slice(options.skip, options.skip ? options.skip + 100 : undefined)
-        .map((x) => {
-          return {
-            type: "series",
-            id: x.show.ids.imdb,
-            name: x.show.title,
-            releaseInfo: x.show.year,
-            poster: "https://simkl.in/posters/" + x.show.poster + "_0.jpg",
-            description: x.show.overview,
-          };
-        });
+      return await Promise.all(
+        list.anime
+          ?.filter((x) => {
+            if (options.genre) {
+              return x.anime_type === options.genre;
+            }
+            return true;
+          })
+          .slice(options.skip, options.skip ? options.skip + 50 : undefined)
+          .map(async (x) => {
+            try {
+              if (!x.show.ids.imdb) {
+                throw new Error("No imdb id");
+              }
+              const data = (
+                await getCinemetaMeta(
+                  x.anime_type === "movie" ? "movie" : "series",
+                  x.show.ids.imdb,
+                )
+              )?.meta;
+
+              return {
+                type: "series",
+                id: x.show.ids.imdb,
+                name: x.show.title,
+                releaseInfo: x.show.year,
+                poster: "https://simkl.in/posters/" + x.show.poster + "_0.jpg",
+                ...data,
+              };
+            } catch (e) {
+              return {
+                type: "series",
+                id: x.show.ids.imdb,
+                name: x.show.title,
+                releaseInfo: x.show.year,
+                poster: "https://simkl.in/posters/" + x.show.poster + "_0.jpg",
+              };
+            }
+          }) as any[],
+      );
     }
   } catch (e) {
     console.log(e);
