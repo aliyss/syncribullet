1ad7008231aa711b6a5e80e7fb620dc25f4da6d6
aliyss <silyas.bieri@lagomio.com>
2025-03-03 23:33:54 +0100

    Sentry logging for issues

--- src/utils/auth/stremio.ts
+++ src/utils/auth/stremio.ts
@@ -1,9 +1,9 @@
 export const ALLOWED_ORIGINS = [
   'https://stremio.com',
-  'https://www.stremio.com',
-  'https://app.stremio.com',
-  'https://app.strem.io',
-  'https://web.stremio.com',
+  // 'https://www.stremio.com',
+  // 'https://app.stremio.com',
+  // 'https://app.strem.io',
+  // 'https://web.stremio.com',
 ];
 
 export const ALLOWED_REFERERS = [
